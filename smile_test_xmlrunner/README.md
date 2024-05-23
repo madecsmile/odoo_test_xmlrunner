93e32957c1f21ac9e17553f678f88ef8688632e6
Martin <martin.deconinck@smile.fr>
2024-05-23 09:21:01 +0200

    Initial commit

---
+++ smile_test_xmlrunner/README.md
@@ -0,0 +1,12 @@
+# Unittest xUnit reports
+
+This module generate unittest reports using unittest-xml-reporting tool.
+
+## Requirements
+
+Python library https://pypi.org/project/unittest-xml-reporting/
+
+## Configuration
+
+Add to your odoo Configuration file:
+- **test_result_directory** (default: *test_results*) : The path (created if not exists) where the reports will be written to. 
---
+++ smile_test_xmlrunner/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Unittest xUnit reports",
+    "version": "17.0.0.0.0",
+    "depends": ["base"],
+    "author": "martin.deconinck@smile.fr",
+    "license": "AGPL-3",
+    "description": "This module override Odoo testing method to run them with xmlrunner tool.",
+    "summary": "",
+    "website": "https://smile.eu/fr",
+    "category": "Tools",
+    "sequence": 20,
+    "auto_install": True,
+    "installable": True,
+    "application": False,
+    'external_dependencies': {
+        'python': ['unittest-xml-reporting'],
+    },
+}
